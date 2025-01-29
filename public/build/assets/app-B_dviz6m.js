9486c32413b85ab4cad6f15cce570aa98adbdab6
Rajiv Rjn <hello@rajivrjn.com>
2025-01-29 21:23:07 +0400

    push build

---
+++ public/build/assets/app-B_dviz6m.js
@@ -0,0 +1,1 @@
+$(document).click(function(e){$(e.target).closest(".mobile-menu").length||($(".mobile-menu").removeClass("active"),$(".bars").removeClass("active"))});$("#main_menu .bars").click(function(){$(this).toggleClass("active"),$(".navigation-container").toggleClass("active"),$("body").toggleClass("fixed")});
